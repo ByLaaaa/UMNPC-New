af480fe6a7fdd105bbae35e0af25003b7854d8a3
ByLaaaa <wian.sanjaya0801@gmail.com>
2025-04-23 17:09:19 +0700

    Add: About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,48 @@
 <template>
-    <div>
-    <h2 class="text-2xl font-bold mb-2">Tentang</h2>
-    <p>Ini adalah halaman tentang aplikasi ini.</p>
+    <div class="flex flex-col my-32 justify-center">
+        <div class="text-5xl font-inter font-bold">
+            About UMN Programming Club
+        </div>
+        <div class="font-inter text-gray-500 text-lg mt-4">
+            Learn more about our mission, history, and the community we've built.
+        </div>
+
+        <div class="flex flex-row mx-56 mt-12">
+            <div class="flex flex-col gap-8 w-1/2 mx-12">
+                <div class="text-3xl font-inter font-bold">
+                    Our Story
+                </div>
+                <div class="text-gray-500 font-inter text-justify text-lg mt-4">
+                    Founded in 2012 by three students passionate about competitive programming, the UMN Programming Club began as a small initiative under the  Faculty of informatics at Universitas Multimedia Nusantara. Initially formed by those who actively joined programming competitions, the club started as an informal group of like-minded individuals eager to grow their skills beyond the classroom.
+                    <br />
+                    <br />
+                    Today, UMNPC stands as one of the active student organizations on campus, fostering a culture of continuous learning, innovation, and collaboration. We remain committed to empowering all members on their journey to grow and excel in the world of technology.
+                </div>
+            </div>
+            <!-- timeline -->
+            <ol class="relative border-s border-gray-200 dark:border-gray-700">                  
+                <li class="mb-10 ms-4">
+                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
+                    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+                    <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+              </svg></a>
+                </li>
+                <li class="mb-10 ms-4">
+                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2022</time>
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Marketing UI design in Figma</h3>
+                    <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+                </li>
+                <li class="ms-4">
+                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2022</time>
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">E-Commerce UI code in Tailwind CSS</h3>
+                    <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+                </li>
+            </ol>
+        </div>
     </div>
 </template>
