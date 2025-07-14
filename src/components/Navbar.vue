089adcee97e04fc8cf23f69df43832b28825f3c6
Fabian Dustin Candra <fabiandustin27@gmail.com>
2025-07-14 18:37:29 +0700

    About udah cocok figma ama responsive dikit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -22,7 +22,7 @@
                         <router-link to="/Programs" class="block py-2 px-3 text- rounded-sm hover:bg-gray-100 font-inter md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Programs</router-link>
                     </li>
                     <li>
-                        <router-link class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent font-inter md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</router-link>
+                        <router-link to="/About" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent font-inter md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</router-link>
                     </li>
                     <li>
                         <router-link class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent font-inter md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Committee</router-link>
